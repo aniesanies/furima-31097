ccc3eb05a053a00880e03fd967163dd33b36c624
aniesanies <72492974+aniesanies@users.noreply.github.com>
2020-11-06 19:40:46 +0900

    ログインしていないユーザーのリダイレクト及びルーティングの再設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,13 @@ class ItemsController < ApplicationController
   def params_item
     params.require(:item).permit(:name, :explain, :category_id, :status_id, :delivery_pay_id, :delivery_area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
-  
+
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'items/index'
-  root to: "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :users, only: [:get, :show]
+  root "items#index"
+  resources :items
 end
