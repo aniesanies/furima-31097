842dd9ca57badfae4c4d3f03e16cf9951dea8f35
aniesanies <72492974+aniesanies@users.noreply.github.com>
2020-11-14 18:15:38 +0900

    Revert "formオブジェクトの作成"
    This reverts commit 1f9a5e3db215ea8c244efd19e72b9a10dbcf5856.

--- db/migrate/20201114090929_create_order_adresses.rb
+++
@@ -1,8 +0,0 @@
-class CreateOrderAdresses < ActiveRecord::Migration[6.0]
-  def change
-    create_table :order_adresses do |t|
-
-      t.timestamps
-    end
-  end
-end
--- spec/factories/order_adresses.rb
+++
@@ -1,4 +0,0 @@
-FactoryBot.define do
-  factory :order_adress do
-  end
-end
