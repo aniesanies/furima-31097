67d15faabfc81f1a11fb6220c3829be05bba5eae
aniesanies <72492974+aniesanies@users.noreply.github.com>
2020-11-14 20:10:36 +0900

    rubocopの実行

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,7 +5,7 @@ class OrderAddress
   with_options presence: true do
     validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフン(-)を含んで記述してください' }
-    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください'}
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'は10桁か11桁の半角数字でハイフン(-)なしで入力してください' }
