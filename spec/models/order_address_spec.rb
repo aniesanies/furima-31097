ff225e282b09badc637cd9ac7cfef7288d1262fa
aniesanies <72492974+aniesanies@users.noreply.github.com>
2020-11-16 17:46:05 +0900

    ja.ymlの記述

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe orderAddress, type: :model do
+RSpec.describe OrderAddress, type: :model do
   before do
     @order_address = FactoryBot.build(:order_address)
   end
