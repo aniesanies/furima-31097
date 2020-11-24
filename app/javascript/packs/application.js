82351ab950bc40ccc7a90467b55f34e860d3abe3
aniesanies <72492974+aniesanies@users.noreply.github.com>
2020-11-24 17:19:02 +0900

    画像プレビュー機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../card")
 require("../calculation")
+require("../preview")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
